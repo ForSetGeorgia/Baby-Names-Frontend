81509007216dcde6cf99012c0020401defaa8537
jason <jason.addie@gmail.com>
2018-06-19 08:55:09 +0400

    added pagination to search

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,10 +1,14 @@
 // get the search from the url querystring
 var q = getQueryString('q');
+var page = getQueryString('page');
+if (page == null || page == ''){
+  page = 1;
+}
 
 $('form input#q').attr('value', q);
 
 // get the name details
-make_ajax_call(generate_api_url('search'), {q: q}, function(data){
+make_ajax_call(generate_api_url('search'), {q: q, page: page}, function(data){
   var meta = data.meta;
   data = data.years;
 
@@ -19,4 +23,32 @@ make_ajax_call(generate_api_url('search'), {q: q}, function(data){
     ['name', 'gender', 'overall_rank', 'amount', 'amount_year_change'],
     [{'column': 'name', 'url': './name?id=', 'param_key': 'id'}]);
 
+  // update pagination links
+  var params = 'q=' + q;
+  // first
+  if (meta.current_page == 1){
+    $('.pagination a:eq(0)').bind('click', false).removeAttr("href").addClass('disabled');
+  }else{
+    $('.pagination a:eq(0)').attr('href', $('.pagination a:eq(0)').attr('href') + params + '&page=1');
+  }
+  // prev
+  if (meta.prev_page == null){
+    $('.pagination a:eq(1)').bind('click', false).removeAttr("href").addClass('disabled');
+  }else{
+    $('.pagination a:eq(1)').attr('href', $('.pagination a:eq(1)').attr('href') + params + '&page=' + meta.prev_page);
+  }
+
+  // next
+  if (meta.next_page == null){
+    $('.pagination a:eq(2)').bind('click', false).removeAttr("href").addClass('disabled');
+  }else{
+    $('.pagination a:eq(2)').attr('href', $('.pagination a:eq(2)').attr('href') + params + '&page=' + meta.next_page);
+  }
+
+  // last
+  if (meta.current_page == meta.total_pages){
+    $('.pagination a:eq(3)').bind('click', false).removeAttr("href").addClass('disabled');
+  }else{
+    $('.pagination a:eq(3)').attr('href', $('.pagination a:eq(3)').attr('href') + params + '&page=' + meta.total_pages);
+  }
 });
